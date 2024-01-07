4cf4924e1c2e279b906e0a56a919b7a7ad1c2b7b
Mike Mariano <52498452+mikemariano19@users.noreply.github.com>
2024-01-07 23:36:07 +0800

    Adding input and button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,54 @@
 import * as React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
+
+
 import Container from '@mui/material/Container';
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
+import Grid from '@mui/material/Grid'
+import AddIcon from '@mui/icons-material/Add';
+
 
 function App() {
 
+  
+
   return (
     <>
-      <Container maxWidth="lg">
-        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+      <Container maxWidth="md" disableGutters={true}>
+        <h1>TODO-LIST</h1>
+        <Grid container spacing={1} sx={{height: 1 }}>
+          <Grid item xs={10} sm={10}>
+              <TextField
+                id="standard-password-input"
+                label="ADD TASK"
+                type="password"
+                autoComplete="current-password"
+                variant="standard"
+                sx={{
+                  width: "100%"
+
+                }}
+              />
+          </Grid>
+          <Grid item xs={2} display={{xs: 'block', sm: 'none'}} >
+            <Button variant="contained" color="primary" startIcon={<AddIcon />}
+            sx={{
+              padding: 2,
+            }}
+            >
+            </Button>
+          </Grid>
+          <Grid item md={2} display={{xs: 'none', sm: 'block'}}>
+            <Button variant="contained" color="primary" 
+            sx={{
+              padding: 2,
+              width: 1
+            }}
+            >
+              ADD TASK
+            </Button>
+          </Grid>
+        </Grid>
       </Container>
     </>
   )
